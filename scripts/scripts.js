f542de952e7ab98986c3004f3e1cc44e5b500bb3
Natan <natan_v_barbosa@estudante.sesisenai.org.br>
2026-04-08 11:15:32 -0300

    add- Imagem de fundo

---
+++ scripts/scripts.js
@@ -0,0 +1,16 @@
+document.getElementById("formAdocao").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let moradia = document.getElementById("moradia").value;
+    let idade = document.getElementById("idade").value;
+    let telefone = document.getElementById("telefone").value;
+    let quintal = document.querySelector('input[nome="quintal"]:checked');
+
+    if(nome,length < 3) return alert("Nome Inválido");
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome:" + nome;
+
+    if(idade,length < 18) return alert("")
+
+})
