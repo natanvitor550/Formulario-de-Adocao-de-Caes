7b4deb10b636af5ee4f133d65fcc268ea8b83a0a
Natan <natan_v_barbosa@estudante.sesisenai.org.br>
2026-04-08 11:56:53 -0300

    add- Minimos de numeros

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,6 +11,10 @@ document.getElementById("formAdocao").addEventListener("submit", function (e) {
     if(nome,length < 3) return alert("Nome Inválido");
     document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome:" + nome;
 
-    if(idade,length < 18) return alert("")
+    if(idade,length < 18) return alert("Idade não aceita");
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Idade:" + idade;
+
+     if(telefone,length < 18) return alert("Numero não aceito");
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Telefone:" + telefone;
 
 })
